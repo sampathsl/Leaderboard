691be286f417c3d96b44092fc2c23f09763611d6
sampathSL <sampath.thennakoon@gmail.com>
2015-03-03 20:07:32 +0530

    Change the structure

---
+++ client/lib/leaderboard.js
@@ -0,0 +1,30 @@
+/**
+ * Created by test on 3/3/15.
+ */
+
+if (Meteor.isClient) {
+    Template.leaderboard.players = function () {
+        return Players.find({}, {sort: {score: -1, name: 1}});
+    };
+
+    Template.leaderboard.selected_name = function () {
+        var player = Players.findOne(Session.get("selected_player"));
+        return player && player.name;
+    };
+
+    Template.player.selected = function () {
+        return Session.equals("selected_player", this._id) ? "selected" : '';
+    };
+
+    Template.leaderboard.events({
+        'click input.inc': function () {
+            Players.update(Session.get("selected_player"), {$inc: {score: 5}});
+        }
+    });
+
+    Template.player.events({
+        'click': function () {
+            Session.set("selected_player", this._id);
+        }
+    });
+}
---
+++ client/lib/subscriptor.js
@@ -0,0 +1,5 @@
+/**
+ * Created by test on 3/3/15.
+ */
+//Meteor.subscribe('showItemCount');
+
